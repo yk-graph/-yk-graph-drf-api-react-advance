9189c403095e597c738a0d257297e91e5e7ff01c
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-13 08:09:17 +0900

    feat: タスク新規作成

--- src/features/task/TaskList.js
+++ src/features/task/TaskList.js
@@ -2,15 +2,19 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectTasks } from "./taskSlice";
 import TaskItem from "./TaskItem";
+import TaskInput from "./TaskInput";
 
 const TaskList = () => {
   const tasks = useSelector(selectTasks);
   return (
-    <ul>
-      {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} />
-      ))}
-    </ul>
+    <>
+      <TaskInput />
+      <ul>
+        {tasks.map((task) => (
+          <TaskItem key={task.id} task={task} />
+        ))}
+      </ul>
+    </>
   );
 };
 
