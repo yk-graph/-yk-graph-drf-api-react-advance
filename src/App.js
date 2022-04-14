7ac550e8d1b297b2b38b6ca8068649eebaa3ff92
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-14 12:00:22 +0900

    feat: Appコンポーネント

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import './App.css'
+
+import styles from './App.module.css'
+import { FaSignOutAlt } from 'react-icons/fa'
 
 const App = () => {
+  const logout = () => {
+    localStorage.removeItem('localJWT')
+    window.location.href = '/'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className={styles.containerTasks}>
+      <div className={styles.appTasks}>
+        <button onClick={logout} className={styles.signBtn}>
+          <FaSignOutAlt />
+        </button>
+      </div>
+      <div className={styles.appDetails}></div>
     </div>
   )
 }
